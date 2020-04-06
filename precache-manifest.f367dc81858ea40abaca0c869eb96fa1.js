292d4cf73f1149ff67adfcf2f00670520075e1e9
Dmitry <d.chernolaskin@int.rosohranatelecom.ru>
2020-04-06 16:34:02 +0300

    Updates

---
+++ precache-manifest.f367dc81858ea40abaca0c869eb96fa1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3614e868eac0317476b4a4ffacea7381",
+    "url": "/sc/index.html"
+  },
+  {
+    "revision": "a67d3b3ea92aa419137e",
+    "url": "/sc/static/css/3.bd2fe9d9.chunk.css"
+  },
+  {
+    "revision": "60e9330d85f8d98a7765",
+    "url": "/sc/static/css/main.6c4b4643.chunk.css"
+  },
+  {
+    "revision": "4667c20829e877432e34",
+    "url": "/sc/static/js/2.f1251e66.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/sc/static/js/2.f1251e66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a67d3b3ea92aa419137e",
+    "url": "/sc/static/js/3.08fdab4c.chunk.js"
+  },
+  {
+    "revision": "60e9330d85f8d98a7765",
+    "url": "/sc/static/js/main.94c9d818.chunk.js"
+  },
+  {
+    "revision": "01311f9032e94383f492",
+    "url": "/sc/static/js/runtime-main.aa7110b5.js"
+  },
+  {
+    "revision": "910df377dbd9655083562d647fd08cae",
+    "url": "/sc/static/media/6.910df377.gif"
+  },
+  {
+    "revision": "ae6dd9fd779de1dc5cdf1ab51f4f3a82",
+    "url": "/sc/static/media/user-icon.ae6dd9fd.png"
+  }
+]);
